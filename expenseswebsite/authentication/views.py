fb5659bb7282ea51409554ad27d20dbbd013f968
Viktor1732 <programmer.kras@gmail.com>
2023-05-13 20:29:17 +0700

    Прописал валидацию для поля 'Имя пользователя' (При вводе имени пользователя, проверяются совпадение имени и корректность символов).

--- expenseswebsite/authentication/views.py
+++ expenseswebsite/authentication/views.py
@@ -1,5 +1,24 @@
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse(
+                {'username_error': 'Имя пользователя должно содержать только строчные буквы!'}, status=400
+            )
+        if User.objects.filter(username=username).exists():
+            return JsonResponse(
+                {'username_error': 'Имя пользователя уже существует. Выберите другое имя!'}, status=409
+            )
+        return JsonResponse({'username_valid': True})
 
 
 class RegistrationView(View):
